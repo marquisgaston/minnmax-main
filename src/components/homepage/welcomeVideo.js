c8059e9050899d650661a6d28214adb71b6b5380
mjg1123 <marquisgbusiness@gmail.com>
2020-01-23 23:12:12 -0500

    commit 500 lol

--- src/components/homepage/welcomeVideo.js
+++ src/components/homepage/welcomeVideo.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
+import windowSize from 'react-window-size';
 
 class WelcomeVideo extends Component {
     state = {  }
     render() { 
-        return ( 
-            <div className="welcome-video">
-                <div>
-                    <h3>INTRODUCING MINNMAX</h3>
-                </div>
-                {/* autoplay; */}
-                <ReactPlayer playing="true" url ='ttps://youtu.be/BWgSxte-cMs' autoplay="true"/>
-            
+        const renderContent = () => {
+            if (this.props.windowWidth > 770) {
+                return ( 
+                    <div className="welcome-video">
+                        <div>
+                            <h3>INTRODUCING MINNMAX</h3>
+                        </div>
+                        {/* autoplay; */}
+                        <ReactPlayer playing="true" url ='ttps://youtu.be/BWgSxte-cMs' autoplay="true"/>
+                    
+                    </div>
+                 );
+            }
+        }
+
+        return (
+            <div>
+                {renderContent()}
             </div>
-         );
+        )
     }
 }
  
-export default WelcomeVideo;
+export default windowSize(WelcomeVideo);
